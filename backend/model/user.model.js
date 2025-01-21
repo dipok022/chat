e1f065249cd1d5604c0a1629eb350dd481187b82
Dipok Roy <84003272+dipok022@users.noreply.github.com>
2025-01-21 23:53:45 +0600

    user model update

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -3,32 +3,20 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Name must be fill"],
+    required: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "It's not valid email address",
-    ],
     lowercase: true,
   },
   password: {
     type: String,
-    required: [true, "Password must be fill"],
+    required: true,
     minlength: 8,
-    validate: {
-      validator: (value) =>
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-          value
-        ),
-      message:
-        "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character",
-    },
   },
 });
 
-const userModel = module.exports("User", userSchema);
+const userModel = mongoose.model("User", userSchema);
 export default userModel;
