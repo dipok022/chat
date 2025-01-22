49e5132c20fa7d55df08c95467773ad61771e2a6
dipok <roydipok022@gmail.com>
2025-01-22 16:50:12 +0600

    update folder name

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./router/user.js";
+import userRouter from "./routers/user.js";
 
 // middleware
 const app = express();
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import userModel from "../model/user.model.js";
+import userModel from "../models/user.model.js";
 
 const router = express.Router();
 
