1abe5d2671044cfc9488c67854480bf4c388fc73
dipok <roydipok022@gmail.com>
2025-01-22 17:21:38 +0600

    update

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,25 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routers/user.js";
+import userRouter from "./routers/user.router.js";
+import connectDB from "./db/index.js";
 
 // middleware
 const app = express();
 app.use(express.json());
-dotenv.config();
-
-// connect database
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log("Connected to MongoDB");
+dotenv.config({
+  path: "./.env",
 });
 
 // routes
 app.use("/api/v1", userRouter);
 
-// listen server port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// connect database and listen port
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("db connection failed !!! ", err);
+  });
