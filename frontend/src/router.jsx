e81ec08031f2bb1487eea74bc502f84678af337d
Dipok Roy <84003272+dipok022@users.noreply.github.com>
2025-02-11 21:43:33 +0600

    login page added

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "./pages/auth/register";
+import Login from "./pages/auth/login";
 
 export const router = createBrowserRouter([
   { path: "/", element: <div>Home</div> },
   { path: "/app/register", element: <Register /> },
+  { path: "/app/login", element: <Login /> },
 ]);
